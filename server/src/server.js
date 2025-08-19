3efbe0e40f5ae848d81790bbe3ad3cb7ed25f959
Vikas Jha <vjha6510@gmail.com>
2025-08-19 13:17:03 +0530

    Integrtaed Socket.io in backend

--- server/src/server.js
+++ server/src/server.js
@@ -2,8 +2,16 @@ import express from "express";
 import { connectDB } from "./config/dbConfig.js";
 import { MONGO_URL } from "./config/serverConfig.js";
 import userRoutes from "./Routes/userRoutes.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
 // Middlewares
 app.use(express.json());
@@ -12,7 +20,7 @@ app.use("/user", userRoutes);
 
 const PORT = 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(` Server is running on port ${PORT}`);
   connectDB();
 });
